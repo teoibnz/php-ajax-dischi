cd0a5369ce9bf2522fc9614cb3e6a5321bd4a740
Matteo Lopez <matteolopezbb@gmail.com>
2022-04-26 09:04:42 +0200

    inizializzazione file e creazione sezioni varie

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
+    <title>Document</title>
+</head>
+<body>
+<?php 
+    include __DIR__ . '/server/data.php';
+    include __DIR__ .  '/'
+?>    
+    <main id='app'>
+
+    </main>
+
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <script src="js/script.js"></script>
+</body>
+</html>
